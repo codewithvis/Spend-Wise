2ad9914bc4483fb75536d8351dc979bf81c69ecf
VISHAL <vrao717305@gmail.com>
2025-09-27 22:02:23 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("dark", fontInter.variable)}>
+    <html lang="en" className={cn("dark", fontInter.variable)} suppressHydrationWarning>
       <body>
         <FirebaseClientProvider>
           <Providers>{children}</Providers>
