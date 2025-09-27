e44e8f2fe941f63d85cc15a3ca8cfa552546bd62
VISHAL <vrao717305@gmail.com>
2025-09-27 22:07:44 +0000

    apply this image to the login page also

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -42,7 +42,7 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
+    <div className="flex min-h-screen flex-col items-center justify-center p-4">
       <div className="w-full max-w-md">
         <div className="mb-8 flex justify-center">
           <Logo />
