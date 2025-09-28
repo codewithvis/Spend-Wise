4308280439b7573a64db2f2f75066fc560baf8ce
VISHAL <vrao717305@gmail.com>
2025-09-28 18:03:11 +0000

    We have detected a publicly accessible Google API key associated with th

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
   "projectId": "studio-3595285736-2e3dd",
   "appId": "1:771619930071:web:61aa93af0f5efe339bd592",
-  "apiKey": "AIzaSyDa86t1ULmNysthXSHHY84G4pOqhNcoZh8",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-3595285736-2e3dd.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "771619930071"
 };
+
